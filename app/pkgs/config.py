60a9eb59a517d68d59709edfd7a44bd0bd52cf67
attapon-th <attapon_srem@hotmail.com>
2025-07-30 11:48:08 +0700

    Refactor Config class: remove unused imports and comments, update APP_TITLE initialization

--- app/pkgs/config.py
+++ app/pkgs/config.py
@@ -2,7 +2,6 @@ import os
 from dataclasses import dataclass
 from functools import cache
 from pathlib import Path
-from nicegui import ui
 
 @dataclass
 class Config:
@@ -10,6 +9,8 @@ class Config:
     OAUTH2_CLIENT_ID: str
     OAUTH2_CLIENT_SECRET: str
     OAUTH2_METADATA_URL: str
+
+    APP_TITLE: str
     APP_HOST: str
     APP_PORT: int
     APP_SECRET: str
@@ -18,13 +19,14 @@ class Config:
     PROJECT_PATH: Path
     STORAGE_PATH: Path
 
-    APP_TITLE: str = "NiceGUI App"
 
     def __init__(self):
         self.DEVMODE = os.getenv("DEVMODE", "1").lower() == "1"
         self.OAUTH2_CLIENT_ID = os.getenv("OAUTH2_CLIENT_ID", "")
         self.OAUTH2_CLIENT_SECRET = os.getenv("OAUTH2_CLIENT_SECRET", "")
         self.OAUTH2_METADATA_URL = os.getenv("OAUTH2_METADATA_URL", "")
+
+        self.APP_TITLE = os.getenv("APP_TITLE", "Nicegui OAuth2")
         self.APP_PORT = int(os.getenv("APP_PORT", 8080))
         self.APP_HOST = os.getenv("APP_HOST", "localhost")
         self.APP_SECRET = os.getenv("APP_SECRET", "")
@@ -36,19 +38,6 @@ class Config:
 
         
 
-
-    # def url_path(self, path: str) -> str:
-    #     """
-    #     Constructs a full URL path by joining the APP_BASEPATH with the provided path.
-    #     """
-    #     u = f"{self.APP_BASEPATH}{path}"
-    #     return u
-    
-    # def navigate_to(self, path: str) -> None:
-    #     ui.navigate.to(self.url_path(path))
-        
-
-        
 
 @cache
 def get_config() -> Config:
