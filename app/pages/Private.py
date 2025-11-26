8a164fa87c5c287d2f39db45f030ed006e49140a
attapon-th <attapon_srem@hotmail.com>
2025-11-26 11:06:45 +0700

    Refactor Private page view function: rename and simplify code structure

--- app/pages/Private.py
+++ app/pages/Private.py
@@ -5,15 +5,9 @@ from datetime import datetime
 # from app.pkgs.config import Config, get_config
 
 
-@ui.refreshable
-async def View(request: Request, _loads=False) -> None:
-    # conf: Config = get_config()
-    # from .layout import menu
-    # await menu.menu(request)
+async def ViewPrivate(request: Request) -> None:
 
     ui.label(f"Date: {datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')}").classes("text-sm text-gray-500")
-    if not _loads:
-        return
-    with ui.card().classes("w-full max-w-2xl"):
+    with ui.card().classes("w-full mx-auto"):
         ui.label("Private Page").classes("text-h4")
         ui.label("This is a private page.").classes("text-h6")
