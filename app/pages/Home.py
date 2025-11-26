eccbfe84c838ed6629bfc747c87edcc4c0bfdcdd
attapon-th <attapon_srem@hotmail.com>
2025-11-26 11:06:41 +0700

    Update HomePage layout: add application logo and improve user navigation

--- app/pages/Home.py
+++ app/pages/Home.py
@@ -18,10 +18,14 @@ def navigate_to(path: str):
 async def HomePage(request: Request) -> None:
     user_data = await get_userdata(request)
     if user_data is not None:
-        with ui.header().classes("bg-none items-center py-2"):
-            ui.avatar("favorite_border")
-            ui.label(conf.APP_TITLE).classes("text-h5 ml-2")
+        # with ui.header().classes("bg-transparent items-center py-2"):
+        #     ui.avatar("favorite_border")
+        #     ui.label(conf.APP_TITLE).classes("text-h5 ml-2")
         with ui.left_drawer().classes(" items-start"):
+            with ui.row().classes("w-full items-center"):
+                ui.avatar("img:./static/logo.png").classes("ml-2")
+                ui.label(conf.APP_TITLE).classes("text-h5 ml-2")
+            ui.separator()
             ui.button("Home", on_click=navigate_to("/"), color="primary", icon="home").props("flat").classes("w-full items-start")
             ui.separator()
             # menu
@@ -37,14 +41,18 @@ async def HomePage(request: Request) -> None:
                 ui.markdown(f"Logged in as `{userinfo.get('preferred_username', 'User')}`  \nName: `{userinfo.get('name', 'User')}`").classes("text-subtitle2")
                 ui.button("Logout", on_click=navigate_to("/logout"), color="negative", icon="logout").classes("w-full items-start mt-auto")
                 ui.separator()
+        # with ui.footer().classes("bg-primary mt-12 items-center align-center py-1"):
+        #     ui.label(f"© {datetime.now().year} {conf.APP_TITLE}").classes("text-white")
 
         # add sub-page view
+        from app.pages.Private import ViewPrivate
+        
         ui.sub_pages(
             {
                 "/": lambda: ViewIndex(request, user_data),
-                "/private": lambda: ui.label("This is a private page only for logged-in users.").classes("text-h5 mt-12"),
+                "/private": lambda: ViewPrivate(request),
             }
-        )
+        ).classes("mt-4 w-full")
 
     else:
         await ViewNoLogin()
@@ -73,5 +81,4 @@ async def ViewIndex(request: Request, user_data: dict) -> None:
             # ui.json_editor({"content": {"json": user_data}}).classes("my-4")
             # ui.button("Logout", on_click=navigate_to("/logout"), color="negative", icon="logout")
 
-    with ui.footer().classes("bg-primary mt-12 items-center align-center py-1"):
-        ui.label(f"© {datetime.now().year} {conf.APP_TITLE}").classes("text-white")
+
